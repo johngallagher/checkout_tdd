b28f73b45ab7e6206a9aa5c324f8db04d3929f57
johngallagher <john@synapticmishap.co.uk>
2023-05-08 22:03:51 +0000

    fix: change behaviour

--- src/test/java/com/sainsburys/AppTest.java
+++ src/test/java/com/sainsburys/AppTest.java
@@ -9,35 +9,32 @@ import java.math.BigDecimal;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
+    public void testApp() {
         App app = new App();
         app.scan("tshirt");
-        BigDecimal expectedTotal = new BigDecimal("2.60");
+        app.scan("mug");
+        BigDecimal expectedTotal = new BigDecimal("5.70");
         assertEquals(expectedTotal, app.calculateTotal());
     }
 }
