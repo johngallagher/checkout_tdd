dfc2b43e4f85b20490868bf200a2d975498a231f
johngallagher <john@synapticmishap.co.uk>
2023-05-09 00:10:16 +0000

    chore: reset ready for an attempt

--- src/main/java/com/sainsburys/App.java
+++ src/main/java/com/sainsburys/App.java
@@ -1,49 +1,8 @@
 package com.sainsburys;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
+public class App {
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    private final ArrayList<String> products;
+  public static void main(String[] products) {
+  }
 
-    public App() {
-        this.products = new ArrayList<String>();
-    }
-
-    public static void main(String[] products )
-    {
-        App app = new App();
-        for (String product : products) {
-            app.scan(product);
-        }
-        System.out.println("Total is:");
-        System.out.println("£" + app.calculateTotal());
-    }
-
-    public BigDecimal calculateTotal() {
-        BigDecimal total = new BigDecimal(0);
-        for (String product : this.products) {
-            total = total.add(this.getPrice(product));
-        }
-        return total;
-    }
-
-    private BigDecimal getPrice(String product) {
-        if (product.equals("tshirt")) {
-            return new BigDecimal("2.60");
-        } else if (product.equals("mug")) {
-            return new BigDecimal("3.10");
-        } else {
-            throw new RuntimeException("Unknown product: " + product);
-        }
-    }
-
-    public void scan(String product) {
-        this.products.add(product);
-    }
 }
--- src/test/java/com/sainsburys/AppTest.java
+++ src/test/java/com/sainsburys/AppTest.java
@@ -4,38 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import java.math.BigDecimal;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        App app = new App();
-        app.scan("tshirt");
-        app.scan("mug");
-        app.scan("tshirt");
-        BigDecimal expectedTotal = new BigDecimal("8.30");
-        assertEquals(expectedTotal, app.calculateTotal());
-    }
+public class AppTest extends TestCase {
+  public static Test suite() {
+    return new TestSuite(AppTest.class);
+  }
 }
