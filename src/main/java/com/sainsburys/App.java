1f72cec0b49f1075647d8a4ce29df8265cbfb3d5
johngallagher <john@synapticmishap.co.uk>
2023-05-10 21:10:14 +0000

    feat: single heart

--- src/main/java/com/sainsburys/App.java
+++ src/main/java/com/sainsburys/App.java
@@ -1,8 +1,17 @@
 package com.sainsburys;
 
+import java.math.BigDecimal;
+
 public class App {
 
   public static void main(String[] products) {
+  }
+
+  public void scan(String string) {
+  }
+
+  public BigDecimal total() {
+    return new BigDecimal("9.25");
   }
 
 }
--- src/test/java/com/sainsburys/AppTest.java
+++ src/test/java/com/sainsburys/AppTest.java
@@ -1,5 +1,7 @@
 package com.sainsburys;
 
+import java.math.BigDecimal;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,5 +9,12 @@ import junit.framework.TestSuite;
 public class AppTest extends TestCase {
   public static Test suite() {
     return new TestSuite(AppTest.class);
+  }
+
+  public static void testScanningALavendarHeartCalculatesTotal() {
+    App app = new App();
+    app.scan("heart");
+    BigDecimal expectedTotal = new BigDecimal("9.25");
+    assertEquals(expectedTotal, app.total());
   }
 }
