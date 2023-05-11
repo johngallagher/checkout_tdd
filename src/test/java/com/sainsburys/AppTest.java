64e6f32600bd75477361d2fc9d8fc38625a36218
johngallagher <john@synapticmishap.co.uk>
2023-05-11 06:25:08 +0000

    test: tidy up tests

--- src/test/java/com/sainsburys/AppTest.java
+++ src/test/java/com/sainsburys/AppTest.java
@@ -11,40 +11,39 @@ public class AppTest extends TestCase {
     return new TestSuite(AppTest.class);
   }
 
-  public void testScanningALavendarHeartCalculatesTotal() {
+  public void testScanningOneHeartTotalsHeartUnitPrice() {
     App app = new App();
     app.scan("heart");
     assertEquals(new BigDecimal("9.25"), app.total());
   }
 
-  public void testScanningHeartAndCufflinksAddsPricesTogether() {
+  public void testScanningOneTshirtTotalsTshirtUnitPrice() {
     App app = new App();
-    app.scan("heart");
-    app.scan("cufflinks");
-    assertEquals(new BigDecimal("54.25"), app.total());
+    app.scan("tshirt");
+    assertEquals(new BigDecimal("19.95"), app.total());
   }
 
-  public static void testAllowsForCallingMultipleProductsInScan() {
+  public void testScanningOneCufflinksTotalsCufflinksUnitPrice() {
+    App app = new App();
+    app.scan("cufflinks");
+    assertEquals(new BigDecimal("45.00"), app.total());
+  }
+
+  public void testScanningHeartAndCufflinksAddsUnitPricesTogether() {
     App app = new App();
     app.scan("heart,cufflinks");
     assertEquals(new BigDecimal("54.25"), app.total());
   }
 
-  public static void testReducesTwoHeartsTo850EachToSellMore() {
+  public void testScanningCufflinksAndTshirtAddsUnitPricesTogether() {
+    App app = new App();
+    app.scan("heart,tshirt");
+    assertEquals(new BigDecimal("29.20"), app.total());
+  }
+
+  public void testDiscountsTwoOrMoreHeartsToSellMore() {
     App app = new App();
     app.scan("heart,heart");
     assertEquals(new BigDecimal("17.00"), app.total());
-  }
-  
-  public static void testReducesThreeHeartsTo850EachToSellMore() {
-    App app = new App();
-    app.scan("heart,heart,heart");
-    assertEquals(new BigDecimal("25.50"), app.total());
-  }
-
-  public static void testTotalsUpHeartsCufflinksAndTshirts() {
-    App app = new App();
-    app.scan("heart,cufflinks,tshirt");
-    assertEquals(new BigDecimal("74.20"), app.total());
   }
 }
