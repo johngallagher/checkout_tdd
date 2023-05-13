96995d51f539fe6c143e0581aeee81f9afa1c1ab
johngallagher <john@synapticmishap.co.uk>
2023-05-13 11:11:03 +0000

    feat: second product

--- src/main/java/com/sainsburys/App.java
+++ src/main/java/com/sainsburys/App.java
@@ -5,11 +5,17 @@ public class App {
   public static void main(String[] products) {
   }
 
+  private String product;
+
   public void scan(String product) {
-    
+    this.product = product;
   }
 
   public String calculateTotal() {
-    return "9.25";
+    if (product.equals("heart")){
+      return "9.25";
+    } else {
+      return "45.00";
+    }
   }
 }
--- src/test/java/com/sainsburys/AppTest.java
+++ src/test/java/com/sainsburys/AppTest.java
@@ -9,10 +9,17 @@ public class AppTest extends TestCase {
     return new TestSuite(AppTest.class);
   }
 
-  public void testCheckingOutWithASingleHeartGivesATotalOfTheUnitPrice() {
+  public void testScanningHeartGivesATotalOfTheUnitPrice() {
     String heart = "heart";
     App app = new App();
     app.scan(heart);
     assertEquals("9.25", app.calculateTotal());
+  }
+
+  public void testScanningCufflinksGivesATotalOfTheUnitPrice() {
+    String cufflinks = "cufflinks";
+    App app = new App();
+    app.scan(cufflinks);
+    assertEquals("45.00", app.calculateTotal());
   }
 }
